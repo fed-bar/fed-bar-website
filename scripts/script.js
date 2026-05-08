0f0379d1d67573381a5d24fc9b1ea5cdbb7035e8
fed-bar <b.ten.brink@hva.nl>
2026-05-08 13:04:02 +0200

    dark mode en een animatie

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
 // JavaScript Document
 console.log("Hallo Barbara");
+
+const staart = document.querySelector('.staart');
+staart.addEventListener('click', () => {
+staart.classList.add('gevallen');
+});
